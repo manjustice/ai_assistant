24e3c6ca2f1581e2eb3d28204b93227438253fec
manjustice <manjustice1998@gmail.com>
2023-06-21 19:29:02 +0300

    Changed main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 from app.api import api_router
@@ -7,7 +6,3 @@ from app.api import api_router
 app = FastAPI()
 
 app.include_router(api_router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app)
